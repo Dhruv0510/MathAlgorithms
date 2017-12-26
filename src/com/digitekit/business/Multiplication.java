b0024a0c27cb9ff22fe3a00246423ed2c4cdff05
Dhruv0510 <dhruvpatel0510@gmail.com>
2017-12-26 11:37:35 -0600

    Multiplication

---
+++ src/com/digitekit/business/Multiplication.java
@@ -0,0 +1,14 @@
+package com.digitekit.business;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		multiplication();
+	}
+public static void multiplication() {
+	int a = 50;
+	int b = 50;
+	int result = a * b;
+	System.out.println("Value of result: " + result);
+}
+}
