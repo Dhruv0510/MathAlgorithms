645e4773026b3c47a585cc205e69cfcd57c74d90
Dhruv0510 <dhruvpatel0510@gmail.com>
2017-12-26 11:48:30 -0600

    Division

---
+++ src/com/digitekit/business/Division.java
@@ -0,0 +1,15 @@
+package com.digitekit.business;
+
+public class Division {
+
+	public static void main(String[] args) {
+		division();
+		
+	}
+	public static void division() {
+		int a = 30;
+		int b = 3;
+		int result = a / b;
+		System.out.println("Vaule of result: " + result);
+	}
+}
