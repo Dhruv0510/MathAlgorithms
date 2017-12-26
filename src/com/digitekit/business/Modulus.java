f28489c180e2a82b2145448a8e15ef22563aa8b5
Dhruv0510 <dhruvpatel0510@gmail.com>
2017-12-26 11:57:28 -0600

    Modulus

---
+++ src/com/digitekit/business/Modulus.java
@@ -0,0 +1,14 @@
+package com.digitekit.business;
+
+public class Modulus {
+
+	public static void main(String[] args) {
+		modulus();
+		}
+public static void modulus() {
+	int a = 50;
+	int b = 20;
+	int result = a % b;
+	System.out.println("Value of result: " + result);
+	}
+}
