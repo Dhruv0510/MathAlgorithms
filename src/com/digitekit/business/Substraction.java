4d9a72225336dbf8b3f8bdbb9c1798ae9f337268
Dhruv0510 <dhruvpatel0510@gmail.com>
2017-12-26 11:31:08 -0600

    Substraction

---
+++ src/com/digitekit/business/Substraction.java
@@ -0,0 +1,16 @@
+package com.digitekit.business;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		substraction();
+	}
+		
+		public static void substraction () {
+			int a = 50;
+			int b = 90;
+			int result = a - b;
+			System.out.println("Value of result: " + result);
+		}
+	}
+
