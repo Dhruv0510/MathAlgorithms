374fa3d6e3163228f8c9ab46ea1b26129b0897d4
Dhruv0510 <dhruvpatel0510@gmail.com>
2017-12-26 11:05:22 -0600

    Initial commit

---
+++ src/com/digitekit/business/Addition.java
@@ -0,0 +1,17 @@
+package com.digitekit.business;
+
+public class Addition {
+
+	public static void main(String[] args) {
+
+		addition();
+	}
+	//hjt
+	static void addition() {
+		 
+		int a = 30;
+		int b = 40;
+		int result = a + b;
+		System.out.println("value of result: " + result);
+		}
+	}
